90eef8508af7c41c3aacb37eea1f7acabb6852e8
Thomas Gwynne-Timothy <thomas.gt@me.com>
2017-04-22 01:51:58 -0300

    Added Hough Transform analysis and smoothing to generate target drive angle. Just need to add keyboard input and will be ready to test.

--- viper/screen.py
+++ viper/screen.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+import matplotlib.pyplot as plt
 
 
 def auto_canny(image, sigma=0.33, center=None):
@@ -31,7 +32,7 @@ def mask_roi_noise(image, vertices):
     cv2.fillPoly(mask, [vertices], 255)
     image_masked = cv2.bitwise_and(image, mask)
     inv_mask = 255 - mask
-    noise = inv_mask * np.random.rand(np.shape(image)[0], np.shape(image)[1])
+    noise = inv_mask.astype(np.double) * np.random.rand(np.size(image, 0), np.size(image, 1))
     return image_masked + noise.astype(np.uint8)
 
 
@@ -39,35 +40,46 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 startTime = lastTime = currentTime = time.time()
 avgFps = 0
 roi_vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [790, 300], [790, 500]])
-avgThreshold = 127
+avg_threshold = 127
+avg_lines = np.zeros((600, 800), dtype=np.uint8)
+avg_line_angle = 0
+
 
 while True:
     # Capture the screen
     screen = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(8, 30, 808, 630))), cv2.COLOR_RGB2BGR)
 
     # Process the image
-    screenGray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-    screenFilter = cv2.GaussianBlur(screenGray, (5, 5), 0)
-    otsuThreshold = cv2.threshold(screenFilter, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[0]
-    avgThreshold = 0.9 * avgThreshold + 0.1 * otsuThreshold
-    # screenThreshold = cv2.threshold(screenFilter, avgThreshold, 255, cv2.THRESH_BINARY)[1]
-    screenEdge = auto_canny(screenFilter, 1, avgThreshold)
-    screenMask = mask_roi(screenEdge, roi_vertices)
+    screen_gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    screen_filter = cv2.GaussianBlur(screen_gray, (7, 7), 0)
+    # screen_roi_noise = mask_roi_noise(screen_filter, roi_vertices)
+    otsu_threshold = cv2.threshold(screen_filter, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[0]
+    avg_threshold = 0.6 * avg_threshold + 0.3 * otsu_threshold
+    # screenThreshold = cv2.threshold(screen_filter, avg_threshold, 255, cv2.THRESH_BINARY)[1]
+    screen_canny = auto_canny(255 - screen_filter, 0.5, avg_threshold)
+    screenMask = mask_roi(screen_canny, roi_vertices)
 
     # Find lines on the image
-    lines = cv2.HoughLinesP(screenMask, 1, np.pi/180, 1, minLineLength=100, maxLineGap=10)
+    lines = cv2.HoughLinesP(screenMask, 1, np.pi / 180, 80, minLineLength=80, maxLineGap=20)
+    current_lines = np.zeros_like(screen_gray)
+    current_line_angle = 0
+
     if lines is not None:
         for x1, y1, x2, y2 in lines[:, 0, :]:
-            cv2.line(screen, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.line(current_lines, (x1, y1), (x2, y2), 255, 2)
+            cv2.line(screen, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            current_line_angle += np.arctan(np.float64(x2 - x1)/np.float64(y1 - y2))
+        current_line_angle /= np.size(lines, 0)
+
+    avg_lines = 0.5 * avg_lines + 0.5 * current_lines
+    avg_line_angle = 0.7 * avg_line_angle + 0.3 * current_line_angle
+    cv2.line(screen, (400, 600), (int(600*np.tan(avg_line_angle) + 400), 0), (0, 255, 0), 2)
 
     # Display the processed data
-    # cv2.imshow("Gray", screenGray)
-    # cv2.imshow("Filter", screenFilter)
-    cv2.imshow("Edge Detected", screenEdge)
+    cv2.imshow("Edge Detected", screen_canny)
     cv2.imshow("Edge Detected + Mask", screenMask)
-    # cv2.imshow("Threshold", screenThreshold)
-    # cv2.imshow("Threshold1", temp)
-    cv2.imshow("Hough Lines", screen)
+    cv2.imshow("Hough Lines", avg_lines)
+    cv2.imshow("Lanes", screen)
 
     # Calculate the frame rate
     currentTime = time.time()
