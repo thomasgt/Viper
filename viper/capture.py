e2ea60a60e88d20ae5cd55f1345def7cdfb0b68e
Thomas Gwynne-Timothy <thomas.gt@me.com>
2017-05-10 22:11:43 -0300

    Added method to move the window.

--- viper/capture.py
+++ viper/capture.py
@@ -25,6 +25,12 @@ class Window:
         if self.hwnd is not None:
             win32gui.SetForegroundWindow(self.hwnd)
 
+    def move_window(self, x, y):
+        bbox = win32gui.GetWindowRect(self.hwnd)
+        w = bbox[2] - bbox[0]
+        h = bbox[3] - bbox[1]
+        win32gui.MoveWindow(self.hwnd, x, y, w, h, True)
+
     def capture_screen(self):
         bbox = win32gui.GetWindowRect(self.hwnd)
         return cv2.cvtColor(np.array(ImageGrab.grab(bbox)), cv2.COLOR_RGB2BGR)
@@ -32,10 +38,9 @@ class Window:
 
 if __name__ == "__main__":
     gta_window = Window()
-    if gta_window.link_handler("Grand Theft Auto V"):
-        gta_window.focus_window()
-    else:
-        print("Could not find window!")
+    gta_window.link_handler("Grand Theft Auto V")
+    gta_window.focus_window()
+    gta_window.move_window(0, 0)
 
     time.sleep(2)
 
